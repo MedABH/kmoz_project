8b7e90170999b0cf4c5e9064b1c472bf1b0dddab
MedABH <128604732+MedABH@users.noreply.github.com>
2023-07-25 16:26:13 +0100

    after formation

--- src/menu/MenuBar.js
+++ src/menu/MenuBar.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { useState } from "react";
 
 
 function MenuBar() {
@@ -9,7 +8,7 @@ function MenuBar() {
             <nav style={{width:'100%', backdropFilter:'blur(8px)'}} className="fixed-top navbar navbar-expand-lg bg-body-tertiary text-center justify-content-center pt-0">
                 <div style={{backdropFilter:'blur(8px)'}} className="nav justify-content-center text-center pt-0">
                     <Link className="nav-link active px-5" aria-current="page" to="/">
-                        <img style={{width:'80px'}} src="logo.svg"></img>
+                        <img style={{width:'80px'}} src="logo.svg" alt="Logo"/>
                     </Link>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
